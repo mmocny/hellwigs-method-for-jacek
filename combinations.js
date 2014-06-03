905a2db0d85b91687861afcae88f6daf2fb3b868
Michal Mocny <mmocny@gmail.com>
2014-06-03 16:22:11 -0400

    Updating combinations

--- combinations.js
+++ combinations.js
@@ -1,9 +1,37 @@
-module.exports = exports = function combinations(myArray, choose) {
+/*
+$var = array(1,2,3);
+$result = array(); 
+$combination = array();
+$corr_y = array(10,12,14);
+ 
+function inner ($start, $choose_, $arr, $n) {
+    global $result, $combination;
+    if ($choose_ == 0) array_push($result,$combination);
+    else for ($i = $start; $i <= $n - $choose_; ++$i) {
+           array_push($combination, $arr[$i]);
+           inner($i + 1, $choose_ - 1, $arr, $n);
+           array_pop($combination);
+     }
+  }
+ 
+function combinations(array $myArray, $choose) {
+  global $result, $combination;
+  $result=array();
+  $n = count($myArray);
+  inner(0, $choose, $myArray, $n);
+  //echo $result[0][0]."  ".$result[0][1];
+  return $result;
+}
+ */
+
+var assert = require('assert');
+
+module.exports = exports = function combinations(arr, choose) {
   var result = [], combination = [];
 
-  function inner(start, choose, arr) {
+  function inner(start, choose) {
     if (choose == 0) {
-      result = result.concat(combination);
+      result.push(combination);
     } else {
       for (var i = start; i <= (arr.length - choose); ++i) {
         combination.push(arr[i]);
@@ -12,7 +40,9 @@ module.exports = exports = function combinations(myArray, choose) {
       }
     }
   }
-  inner(0, choose, myArray);
+  inner(0, choose);
+
+  assert.ok(combination.length == 0);
 
   return result;
 }
--- hellwigs-method.js
+++ hellwigs-method.js
@@ -42,25 +42,29 @@ module.exports = exports = function hellwigsMethod(correlation, variables, corre
   var combinations = require('./combinations');
   var wyniki = [],
       x = 0;
-  for (var a = 1; a <= variables.length, ++a) {
-    for (var b = 1; b <= combinations(variables, a).length; ++b) {
+
+  for (var a = 1; a <= variables.length; ++a) {
+    var comb_var_a = combinations(variables, a);
+
+    for (var b = 1; b <= comb_var_a.length; ++b) {
       if (a == 1) {
-        // array_push($wyniki,pow($correlation_y[combinations($variables,$a)[$b-1][0]-1],2)); //jezeli mamy tylko jedna zmienna w kombinacji to wsadz od razu do wynikow
+        console.log(comb_var_a[b-1]);
+        wyniki.push(Math.pow(correlation_y[comb_var_a[b-1][0]-1],2)); // jezeli mamy tylko jedna zmienna w kombinacji to wsadz od razu do wynikow
       } else {
         /*
           $poj_wspolna=0;
           $d=0;
-          while($d<count(combinations($variables,$a)[$b-1]))
+          while($d<count(comb_var_a[$b-1]))
             {
             $mianownik=0;
-            for($i=0;$i<count(combinations($variables,$a)[$b-1]);$i++)
+            for($i=0;$i<count(comb_var_a[$b-1]);$i++)
               {
-              $mianownik += abs(floatval($correlation[combinations($variables,$a)[$b-1][$d]-1][combinations($variables,$a)[$b-1][$i]-1])); 
+              $mianownik += abs(floatval($correlation[comb_var_a[$b-1][$d]-1][comb_var_a[$b-1][$i]-1])); 
               }
-            $poj_ind = pow($correlation_y[combinations($variables,$a)[$b-1][$d]-1],2)/$mianownik;
+            $poj_ind = pow($correlation_y[comb_var_a[$b-1][$d]-1],2)/$mianownik;
             $poj_wspolna += $poj_ind;
             $d++;
-            if($d==count(combinations($variables,$a)[$b-1])) array_push($wyniki,$poj_wspolna);
+            if($d==count(comb_var_a[$b-1])) array_push($wyniki,$poj_wspolna);
             }
             */
 
