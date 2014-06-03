96ec59a17c23921c2d11914fdc4cb7189160bf9b
Michal Mocny <mmocny@gmail.com>
2014-06-03 16:08:41 -0400

    starting to implement hellwigs

--- hellwigs-method.js
+++ hellwigs-method.js
@@ -40,9 +40,34 @@ function hellwig($correlation, $variables,$correlation_y)
 
 module.exports = exports = function hellwigsMethod(correlation, variables, correlation_y) {
   var combinations = require('./combinations');
+  var wyniki = [],
+      x = 0;
+  for (var a = 1; a <= variables.length, ++a) {
+    for (var b = 1; b <= combinations(variables, a).length; ++b) {
+      if (a == 1) {
+        // array_push($wyniki,pow($correlation_y[combinations($variables,$a)[$b-1][0]-1],2)); //jezeli mamy tylko jedna zmienna w kombinacji to wsadz od razu do wynikow
+      } else {
+        /*
+          $poj_wspolna=0;
+          $d=0;
+          while($d<count(combinations($variables,$a)[$b-1]))
+            {
+            $mianownik=0;
+            for($i=0;$i<count(combinations($variables,$a)[$b-1]);$i++)
+              {
+              $mianownik += abs(floatval($correlation[combinations($variables,$a)[$b-1][$d]-1][combinations($variables,$a)[$b-1][$i]-1])); 
+              }
+            $poj_ind = pow($correlation_y[combinations($variables,$a)[$b-1][$d]-1],2)/$mianownik;
+            $poj_wspolna += $poj_ind;
+            $d++;
+            if($d==count(combinations($variables,$a)[$b-1])) array_push($wyniki,$poj_wspolna);
+            }
+            */
 
-  for (var a = 1; a <= variables.length; ++a) {
-    console.log(combinations(variables, a));
+      }
+    }
   }
-  return [];
+
+  console.log(wyniki);
+  return x;
 };
